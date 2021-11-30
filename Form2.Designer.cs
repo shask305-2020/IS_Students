ba046842e4d50bd40bcbc1f8caa92c77ac15e476
ШАСК Студент <shask305@hotmail.com>
2021-11-30 23:43:33 +0500

    Resize

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -37,16 +37,16 @@ namespace IS_Students
             this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
             this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
             this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
-            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
             this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
             this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroComboBox2 = new MetroFramework.Controls.MetroComboBox();
             this.SuspendLayout();
             // 
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
             this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel1.Location = new System.Drawing.Point(13, 58);
+            this.metroLabel1.Location = new System.Drawing.Point(20, 69);
             this.metroLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(100, 25);
@@ -57,7 +57,7 @@ namespace IS_Students
             // 
             this.metroLabel2.AutoSize = true;
             this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel2.Location = new System.Drawing.Point(6, 92);
+            this.metroLabel2.Location = new System.Drawing.Point(13, 103);
             this.metroLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.metroLabel2.Name = "metroLabel2";
             this.metroLabel2.Size = new System.Drawing.Size(107, 25);
@@ -68,7 +68,7 @@ namespace IS_Students
             // 
             this.metroLabel3.AutoSize = true;
             this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel3.Location = new System.Drawing.Point(70, 129);
+            this.metroLabel3.Location = new System.Drawing.Point(70, 153);
             this.metroLabel3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.metroLabel3.Name = "metroLabel3";
             this.metroLabel3.Size = new System.Drawing.Size(43, 25);
@@ -79,7 +79,7 @@ namespace IS_Students
             // 
             this.metroLabel4.AutoSize = true;
             this.metroLabel4.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel4.Location = new System.Drawing.Point(54, 166);
+            this.metroLabel4.Location = new System.Drawing.Point(61, 197);
             this.metroLabel4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.metroLabel4.Name = "metroLabel4";
             this.metroLabel4.Size = new System.Drawing.Size(59, 25);
@@ -90,7 +90,7 @@ namespace IS_Students
             // 
             this.metroLabel5.AutoSize = true;
             this.metroLabel5.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel5.Location = new System.Drawing.Point(54, 213);
+            this.metroLabel5.Location = new System.Drawing.Point(54, 241);
             this.metroLabel5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.metroLabel5.Name = "metroLabel5";
             this.metroLabel5.Size = new System.Drawing.Size(59, 25);
@@ -103,66 +103,64 @@ namespace IS_Students
             this.metroTextBox1.Location = new System.Drawing.Point(129, 64);
             this.metroTextBox1.Margin = new System.Windows.Forms.Padding(2);
             this.metroTextBox1.Name = "metroTextBox1";
-            this.metroTextBox1.Size = new System.Drawing.Size(327, 19);
+            this.metroTextBox1.Size = new System.Drawing.Size(327, 30);
             this.metroTextBox1.TabIndex = 5;
             // 
             // metroTextBox2
             // 
             this.metroTextBox2.FontSize = MetroFramework.MetroTextBoxSize.Tall;
-            this.metroTextBox2.Location = new System.Drawing.Point(129, 98);
+            this.metroTextBox2.Location = new System.Drawing.Point(129, 103);
             this.metroTextBox2.Margin = new System.Windows.Forms.Padding(2);
             this.metroTextBox2.Name = "metroTextBox2";
-            this.metroTextBox2.Size = new System.Drawing.Size(327, 19);
+            this.metroTextBox2.Size = new System.Drawing.Size(327, 30);
             this.metroTextBox2.TabIndex = 6;
             // 
             // metroTextBox3
             // 
             this.metroTextBox3.FontSize = MetroFramework.MetroTextBoxSize.Tall;
-            this.metroTextBox3.Location = new System.Drawing.Point(129, 213);
+            this.metroTextBox3.Location = new System.Drawing.Point(129, 236);
             this.metroTextBox3.Margin = new System.Windows.Forms.Padding(2);
             this.metroTextBox3.Name = "metroTextBox3";
-            this.metroTextBox3.Size = new System.Drawing.Size(327, 38);
+            this.metroTextBox3.Size = new System.Drawing.Size(327, 30);
             this.metroTextBox3.TabIndex = 7;
-            // 
-            // metroCheckBox1
-            // 
-            this.metroCheckBox1.AutoSize = true;
-            this.metroCheckBox1.FontSize = MetroFramework.MetroLinkSize.Tall;
-            this.metroCheckBox1.Location = new System.Drawing.Point(129, 129);
-            this.metroCheckBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.metroCheckBox1.Name = "metroCheckBox1";
-            this.metroCheckBox1.Size = new System.Drawing.Size(36, 25);
-            this.metroCheckBox1.TabIndex = 8;
-            this.metroCheckBox1.Text = "?";
-            this.metroCheckBox1.UseVisualStyleBackColor = true;
             // 
             // metroComboBox1
             // 
             this.metroComboBox1.FormattingEnabled = true;
             this.metroComboBox1.ItemHeight = 23;
-            this.metroComboBox1.Location = new System.Drawing.Point(129, 166);
+            this.metroComboBox1.Location = new System.Drawing.Point(129, 193);
             this.metroComboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.metroComboBox1.Name = "metroComboBox1";
-            this.metroComboBox1.Size = new System.Drawing.Size(84, 29);
+            this.metroComboBox1.Size = new System.Drawing.Size(216, 29);
             this.metroComboBox1.TabIndex = 9;
             // 
             // metroButton1
             // 
-            this.metroButton1.Location = new System.Drawing.Point(373, 286);
+            this.metroButton1.Location = new System.Drawing.Point(373, 295);
             this.metroButton1.Margin = new System.Windows.Forms.Padding(2);
             this.metroButton1.Name = "metroButton1";
             this.metroButton1.Size = new System.Drawing.Size(83, 26);
             this.metroButton1.TabIndex = 10;
             this.metroButton1.Text = "Добавить";
             // 
+            // metroComboBox2
+            // 
+            this.metroComboBox2.FormattingEnabled = true;
+            this.metroComboBox2.ItemHeight = 23;
+            this.metroComboBox2.Location = new System.Drawing.Point(129, 149);
+            this.metroComboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.metroComboBox2.Name = "metroComboBox2";
+            this.metroComboBox2.Size = new System.Drawing.Size(84, 29);
+            this.metroComboBox2.TabIndex = 11;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(491, 345);
+            this.ClientSize = new System.Drawing.Size(500, 350);
+            this.Controls.Add(this.metroComboBox2);
             this.Controls.Add(this.metroButton1);
             this.Controls.Add(this.metroComboBox1);
-            this.Controls.Add(this.metroCheckBox1);
             this.Controls.Add(this.metroTextBox3);
             this.Controls.Add(this.metroTextBox2);
             this.Controls.Add(this.metroTextBox1);
@@ -173,7 +171,7 @@ namespace IS_Students
             this.Controls.Add(this.metroLabel1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
-            this.Padding = new System.Windows.Forms.Padding(11, 49, 11, 11);
+            this.Padding = new System.Windows.Forms.Padding(11, 60, 11, 11);
             this.Text = "Информация о студенте";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -190,8 +188,8 @@ namespace IS_Students
         private MetroFramework.Controls.MetroTextBox metroTextBox1;
         private MetroFramework.Controls.MetroTextBox metroTextBox2;
         private MetroFramework.Controls.MetroTextBox metroTextBox3;
-        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
         private MetroFramework.Controls.MetroComboBox metroComboBox1;
         private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox2;
     }
 }
