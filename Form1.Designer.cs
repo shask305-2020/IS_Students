be59ff548315ba238ec21129d2f669bb9e82157d
ШАСК Студент <shask305@hotmail.com>
2021-11-30 10:25:05 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace IS_Students
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mtUpdate = new MetroFramework.Controls.MetroTile();
+            this.mtAdd = new MetroFramework.Controls.MetroTile();
+            this.mtEdit = new MetroFramework.Controls.MetroTile();
+            this.mtDelete = new MetroFramework.Controls.MetroTile();
+            this.mtSave = new MetroFramework.Controls.MetroTile();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mtUpdate
+            // 
+            this.mtUpdate.Location = new System.Drawing.Point(13, 70);
+            this.mtUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mtUpdate.Name = "mtUpdate";
+            this.mtUpdate.Size = new System.Drawing.Size(111, 76);
+            this.mtUpdate.TabIndex = 0;
+            this.mtUpdate.Text = "Обновить";
+            this.mtUpdate.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            // 
+            // mtAdd
+            // 
+            this.mtAdd.Location = new System.Drawing.Point(137, 70);
+            this.mtAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mtAdd.Name = "mtAdd";
+            this.mtAdd.Size = new System.Drawing.Size(112, 76);
+            this.mtAdd.TabIndex = 0;
+            this.mtAdd.Text = "Добавить";
+            this.mtAdd.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.mtAdd.Click += new System.EventHandler(this.mtAdd_Click);
+            // 
+            // mtEdit
+            // 
+            this.mtEdit.Location = new System.Drawing.Point(266, 70);
+            this.mtEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mtEdit.Name = "mtEdit";
+            this.mtEdit.Size = new System.Drawing.Size(146, 76);
+            this.mtEdit.TabIndex = 0;
+            this.mtEdit.Text = "Редактировать";
+            this.mtEdit.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            // 
+            // mtDelete
+            // 
+            this.mtDelete.BackColor = System.Drawing.Color.Red;
+            this.mtDelete.CustomBackground = true;
+            this.mtDelete.Location = new System.Drawing.Point(430, 70);
+            this.mtDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mtDelete.Name = "mtDelete";
+            this.mtDelete.Size = new System.Drawing.Size(118, 76);
+            this.mtDelete.TabIndex = 0;
+            this.mtDelete.Text = "Удалить";
+            this.mtDelete.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            // 
+            // mtSave
+            // 
+            this.mtSave.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.mtSave.CustomBackground = true;
+            this.mtSave.Location = new System.Drawing.Point(566, 70);
+            this.mtSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mtSave.Name = "mtSave";
+            this.mtSave.Size = new System.Drawing.Size(135, 76);
+            this.mtSave.TabIndex = 0;
+            this.mtSave.Text = "Сохранить";
+            this.mtSave.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 172);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(688, 199);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 392);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.mtSave);
+            this.Controls.Add(this.mtDelete);
+            this.Controls.Add(this.mtEdit);
+            this.Controls.Add(this.mtAdd);
+            this.Controls.Add(this.mtUpdate);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(11, 33, 11, 11);
+            this.Text = "Студенты";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile mtUpdate;
+        private MetroFramework.Controls.MetroTile mtAdd;
+        private MetroFramework.Controls.MetroTile mtEdit;
+        private MetroFramework.Controls.MetroTile mtDelete;
+        private MetroFramework.Controls.MetroTile mtSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace IS_Students
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(13, 58);
+            this.metroLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(100, 25);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "ID студента";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(6, 92);
+            this.metroLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(107, 25);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "Полное имя";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel3.Location = new System.Drawing.Point(70, 129);
+            this.metroLabel3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(43, 25);
+            this.metroLabel3.TabIndex = 2;
+            this.metroLabel3.Text = "Пол";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel4.Location = new System.Drawing.Point(54, 166);
+            this.metroLabel4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(59, 25);
+            this.metroLabel4.TabIndex = 3;
+            this.metroLabel4.Text = "Город";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel5.Location = new System.Drawing.Point(54, 213);
+            this.metroLabel5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(59, 25);
+            this.metroLabel5.TabIndex = 4;
+            this.metroLabel5.Text = "Адрес";
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox1.Location = new System.Drawing.Point(129, 64);
+            this.metroTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(327, 19);
+            this.metroTextBox1.TabIndex = 5;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox2.Location = new System.Drawing.Point(129, 98);
+            this.metroTextBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.Size = new System.Drawing.Size(327, 19);
+            this.metroTextBox2.TabIndex = 6;
+            // 
+            // metroTextBox3
+            // 
+            this.metroTextBox3.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox3.Location = new System.Drawing.Point(129, 213);
+            this.metroTextBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.Size = new System.Drawing.Size(327, 38);
+            this.metroTextBox3.TabIndex = 7;
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.FontSize = MetroFramework.MetroLinkSize.Tall;
+            this.metroCheckBox1.Location = new System.Drawing.Point(129, 129);
+            this.metroCheckBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(36, 25);
+            this.metroCheckBox1.TabIndex = 8;
+            this.metroCheckBox1.Text = "?";
+            this.metroCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Location = new System.Drawing.Point(129, 166);
+            this.metroComboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(84, 29);
+            this.metroComboBox1.TabIndex = 9;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(373, 286);
+            this.metroButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(83, 26);
+            this.metroButton1.TabIndex = 10;
+            this.metroButton1.Text = "Добавить";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 345);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Controls.Add(this.metroTextBox3);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form2";
+            this.Padding = new System.Windows.Forms.Padding(11, 49, 11, 11);
+            this.Text = "Информация о студенте";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
